7613834efffbfa8976d2668d5fc2b6ae89aaa5f7
Jason Matzke <jmatzke1@madisoncollege.edu>
2016-03-22 13:53:43 -0500

    Brian's dumb class

--- weatherWebServiceProjectFiles/src/DayOfWeather.java
+++ weatherWebServiceProjectFiles/src/DayOfWeather.java
@@ -3,5 +3,12 @@
  *         Created on 3/22/16.
  */
 public class DayOfWeather {
-    
+
+    // Chance of precip
+    // Cloudiness
+    // Wind
+    // High Temp
+    // Low Temp
+    // Chance of Rain
+
 }
