9918c00f3db013f551f24f0ff971a9b5f1b790e4
Jason Matzke <jmatzke1@madisoncollege.edu>
2016-03-24 13:53:43 -0500

    Adding variables in class

--- weatherWebServiceProjectFiles/src/DayOfWeather.java
+++ weatherWebServiceProjectFiles/src/DayOfWeather.java
@@ -4,11 +4,58 @@
  */
 public class DayOfWeather {
 
+    private String cloudiness;
+    private String windSpeed;
+    private String highTemperatureOfDay;
+    private String lowTemperatureOfDay;
+    private String chanceOfPrecipitation;
 
-    // Cloudiness
-    // Wind
-    // High Temp
-    // Low Temp
-    // Chance of Rain
+    public String getCloudiness() {
+        return cloudiness;
+    }
 
+    public void setCloudiness(String cloudiness) {
+        this.cloudiness = cloudiness;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getHighTemperatureOfDay() {
+        return highTemperatureOfDay;
+    }
+
+    public void setHighTemperatureOfDay(String highTemperatureOfDay) {
+        this.highTemperatureOfDay = highTemperatureOfDay;
+    }
+
+    public String getLowTemperatureOfDay() {
+        return lowTemperatureOfDay;
+    }
+
+    public void setLowTemperatureOfDay(String lowTemperatureOfDay) {
+        this.lowTemperatureOfDay = lowTemperatureOfDay;
+    }
+
+    public String getChanceOfPrecipitation() {
+        return chanceOfPrecipitation;
+    }
+
+    public void setChanceOfPrecipitation(String chanceOfPrecipitation) {
+        this.chanceOfPrecipitation = chanceOfPrecipitation;
+    }
+
+    public String toString() {
+
+        return "Wind Speed: " + getWindSpeed()
+                + "Cloudiness: " + getCloudiness()
+                + "High Temp of Day: " + getHighTemperatureOfDay()
+                + "Low Temp of Day: " + getLowTemperatureOfDay()
+                + "Precip Chance: " + getChanceOfPrecipitation();
+    }
 }
