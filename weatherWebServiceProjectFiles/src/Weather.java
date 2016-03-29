56f6ffed26cab9835c5e24b1293abf98d7a169ce
Brian Woestman <woestmab@gmail.com>
2016-03-29 14:15:23 -0500

    added to lib dir

--- weatherWebServiceProjectFiles/src/Weather.java
+++ weatherWebServiceProjectFiles/src/Weather.java
@@ -1,19 +1,20 @@
-//package
-import jdk.nashorn.internal.objects.annotations.Getter;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
 
-//import javax.ws.rs.path;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.Produces;
+
+import javax.ws.rs.*;
+
 /**
  * Created by scheffs on 3/28/2016.
  */
+
 // The Java class will be hosted at the URI path "/weather"
-//@Path("/weather")
+@Path("/weather")
 public class Weather {
-    //The Java method will process HTTP GET requests
-    //@GET
+//    The Java method will process HTTP GET requests
+    @GET
     // The Java method will produce content identified by the MIME media type "text/plain"
-    //@Produces("text/plain")
+    @Produces("text/plain")
     public String getClichedMessage() {
         // Return some cliched textual content
         return "Hello Weather";
