3cb373d5817c426fb8df201f4bced8c149099832
Jason Matzke <jmatzke1@madisoncollege.edu>
2016-04-12 10:02:08 -0500

    adding tests to dayOfWeatherTest

--- weatherWebServiceProjectFiles/test/com/weatherWebService/DayOfWeatherTest.java
+++ weatherWebServiceProjectFiles/test/com/weatherWebService/DayOfWeatherTest.java
@@ -3,28 +3,37 @@ package com.weatherWebService;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author jjjasonm
  *         Created on 4/8/16.
  */
 public class DayOfWeatherTest {
 
+    private DayOfWeather dayOfWeather;
+    private final String cloudy = "cloudy";
+
     @Before
     public void setUp() throws Exception {
 
-        DayOfWeather day = new DayOfWeather();
-        day.setCloudCover("cloudy");
+        dayOfWeather = new DayOfWeather();
+        dayOfWeather.setCloudCover(cloudy);
 
     }
-
 
     @Test
     public void testSetCloudCover() throws Exception {
 
+        assertNotNull(dayOfWeather.getCloudCover());
+
     }
+
     @Test
     public void testGetCloudCover() throws Exception {
-
+        String test = dayOfWeather.getCloudCover();
+        assertEquals(test,cloudy);
     }
 
     @Test
